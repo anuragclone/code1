a6ff38ac3188ead07b54463d2f22c4b3e65e12ab
anuragclone <142688251+anuragclone@users.noreply.github.com>
2023-08-19 11:18:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+let a = document.getElementById(home)
+let b = document.getElementById(scholarship)
+let c = document.getElementById(notespedia)
+let d = document.getElementById(book)
+let e = document.getElementById(techblog)
+
+let activeBar = a
+
+let x = document.addEventListener('click')
+
+function change(){
+
+
+activeBar.textContent = {
+    x.style = {
+        padding: .2rem;
+        /* padding-top: 2rem; */
+        border-top-left-radius: 1rem;
+        border-top-right-radius: 1rem;
+        background-color: rgb(0, 157, 255);
+        transform: translateY(-20px);
+    }
+}
+
+
+    if (activeBar == a){
+    
+    }
+    
+    else if (activeBar == b){
+    
+    }
+    
+    else if (activeBar == c){
+    
+    }
+    
+    else if (activeBar == d){
+    
+    }
+    
+    else if (activeBar == e){
+    
+    }
+    
+
+
+}
